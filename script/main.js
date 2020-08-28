6ad264df96a1ad2cd3e04106e2230c8ac0e58698
Lenon <lenonlmsv@gmail.com>
2020-08-28 11:39:17 -0300

    Alterações visuais em plataforma de ensino
    Alterações visuais em plataforma de ensino sys academy, construída com moodle em PHP.

---
+++ script/main.js
@@ -0,0 +1,27 @@
+const divFooter = document.createElement('div');
+divFooter.setAttribute('id','divFooter');
+
+const divMedium = document.createElement('div');
+divMedium.setAttribute('id','divMedium');
+
+const divContent = document.createElement('div');
+divContent.setAttribute('id','divContent');
+divContent.innerHTML = "Legal notice";
+
+const divBackTop = document.createElement('div');
+divBackTop.setAttribute('id','divBackTop');
+let h1 = document.createElement('h1');
+h1.innerHTML = "Apagar";
+divBackTop.appendChild(h1);
+
+divMedium.appendChild(divContent);
+divMedium.appendChild(divBackTop);
+
+divFooter.appendChild(divMedium);
+
+document.querySelector('#page-footer').appendChild(divFooter);
+
+const backTop = document.querySelector('#back-to-top').cloneNode(true);
+document.getElementById('back-to-top').outerHTML = ""
+const divFooterButton = document.getElementById('divBackTop');
+document.getElementById('divBackTop').appendChild(backTop);
